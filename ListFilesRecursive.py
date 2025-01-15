22c7e7da400661e823390af36b588f5f656d6859
Travis Farley <106246278+theoddone11@users.noreply.github.com>
2025-01-15 01:59:26 -0600

    1st!

---
+++ ListFilesRecursive.py
@@ -0,0 +1,25 @@
+import os
+
+#script for figuring out why git is rejecting large files
+
+def lsfilescwd(cwd):
+    print(cwd)
+    pwd = ""
+    for entry in os.listdir(cwd):
+        if(entry):
+            cfile = os.path.join(cwd, entry)
+            statinfo = os.stat(cfile)
+            if(os.path.isdir(cfile)):
+                #print(cfile)
+                pwd = cwd
+                os.chdir(cfile)
+                lsfilescwd(str(cfile))
+            else:
+                fsize = statinfo.st_size
+                _mb_ = 1000000
+                if(fsize > (_mb_)):
+                    print(cfile + " " + str(statinfo.st_size / _mb_) + "MB")
+            #lsfilescwd(pwd)
+lsfilescwd(os.getcwd())
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+#script for figuring out why git is rejecting large files
+#convenient for game-devs not wanting to pay for git-lfs
+
+Plan to improve output legibility and maybe even untrack files.
